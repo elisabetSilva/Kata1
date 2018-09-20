d9ba4b1dba710a649abaaa2460266f0318becadc
Eli <Eli@DESKTOP-KRVLN4G>
2018-09-20 12:20:55 +0100

    Kata 1 versión 2 Se ha modificado el código para calcular la edad con Calendar y GregorianCalendar

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,15 @@
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Elisabet", new Date(93,6,21));
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1993, 6, 21);
+        Person person = new Person("Elisabet", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
 }
