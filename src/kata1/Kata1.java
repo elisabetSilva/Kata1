93898c8641db0694d4a8f5330da38b45d9c02a94
Eli <Eli@DESKTOP-KRVLN4G>
2018-09-21 13:24:05 +0100

    Kata 1 versión 3 Se ha modificado el código para calcular la edad haciendo uso de la librería LocalDate

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,14 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date = GregorianCalendar.getInstance();
-        date.set(1993, 6, 21);
+        //Calendar date = GregorianCalendar.getInstance();
+        //date.set(1993, 6, 21);
+        LocalDate date = LocalDate.of(1993, Month.JUNE, 21);
         Person person = new Person("Elisabet", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
